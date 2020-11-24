625a4f12804d184dd0f3927c1e4032169a7f5658
fedaiipek <fedaiipek55@gmail.com>
2020-11-23 22:41:52 -0600

    denemelere devam ediyoruz arkadaşlar3

--- src/denemeler.java
+++ src/denemeler.java
@@ -1,7 +1,10 @@
+import java.sql.SQLOutput;
+
 public class denemeler {
 
     public static void main(String[] args) {
         System.out.println("Denemeler");
         System.out.println("Denemeler2");
+        System.out.println("Denemeler3");
     }
 }
